8cf95695e269319e3dca7bbd042a826be6fcc691
Evan Schneider <evanrs@gmail.com>
2014-09-16 16:11:46 -0400

    Adds codehighlighting.

--- src/createCurriculumManifest/attach-content.js
+++ src/createCurriculumManifest/attach-content.js
@@ -10,12 +10,24 @@ var Q = require('q');
 
 var frontMatter = require('front-matter');
 var marked = require('marked');
+var hljs = require('highlight.js');
 
 var createDom = require('../create-dom');
 
+var renderer = new marked.Renderer();
+renderer.code = function(code, lang) {
+    var highlighted = hljs.highlightAuto(code);
+    return '<pre class="hljs">' + highlighted.value + '</pre>';
+}
+marked.setOptions({
+    sanitize: true,
+    smartLists: true,
+    renderer: renderer
+});
+
+
 var parseMarkdown = function(str) {
     var parsed;
-
 
     // Changes arbitrary -'d separators in the old thinkdown to three ---
     str = str.replace(/\n\s*----+\s*\n/g, '\n---\n');
