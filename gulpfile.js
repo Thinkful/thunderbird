457394b33256f530a77d4dc61c6b50b1c5d2f8fd
Olex Ponomarenko <tholex@gmail.com>
2014-09-24 18:49:37 -0400

    Gulp works well, but ./thinkful2.js doesnt

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var del = require('del');
 var gulp = require('gulp');
-var filter = require('gulp-filter');
 var debug = require('gulp-debug');
+var filter = require('gulp-filter');
+var rename = require('gulp-rename');
 var flatten = require('gulp-flatten');
 var createCurriculumManifest = require('./src/createCurriculumManifest');
 var populateUUIDs = require('./src/populate-uuids');
@@ -16,6 +17,8 @@ var paths = {
 var extensions = require("./asset-extensions")
 var assetsRegex = new RegExp(".*[.](" + extensions.join("|") + ")$");
 
+var sourceDirRegex = new RegExp(paths.source + "\/?");
+
 /* Collects assets folders into one common assets folder */
 gulp.task('assets', function() {
     if (argv["skip-assets"]) {
@@ -23,9 +26,17 @@ gulp.task('assets', function() {
     } else {
        return gulp.src(paths.source + "/**/*")
         .pipe(filter(function(file) {
+            // ./content/1.1.1_Assignment_Name/content.md ->
+            // -- X -- md is not an acceptable extension, filtered out
             return assetsRegex.test(file.path);
         }))
         // .pipe(debug({title: "ASSET"}))
+        .pipe(rename(function(path) {
+            // ./content/1.1.1_Assignment_Name/img.png ->
+            // ./1.1.1_Assignment_Name.img.png
+            path.dirname = path.dirname.replace(sourceDirRegex, "");
+        }))
+        // .pipe(debug({title: "ASSET MOD"}))
         .pipe(gulp.dest(paths.build + "/assets"));
     }
 });
@@ -33,11 +44,16 @@ gulp.task('assets', function() {
 /* Builds curriculum.json from structure and contents */
 gulp.task('tree', function() {
     return gulp.src(paths.source + '/structure.xml')
+        // .pipe(debug({title: "STRUCTURE XML"}))
         .pipe(populateUUIDs())
         .pipe(createCurriculumManifest({
           filename: "curriculum.json"
         }))
-        .pipe(flatten())
+        .pipe(rename(function(path) {
+            // ./content/1.1.1_Assignment_Name/img.png ->
+            // ./1.1.1_Assignment_Name.img.png
+            path.dirname = path.dirname.replace(sourceDirRegex, "");
+        }))
         .pipe(gulp.dest(paths.build));
 });
 
