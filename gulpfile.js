70d5e09033c6bb3ff3c972357774f57d421d863b
Olex Ponomarenko <tholex@gmail.com>
2014-09-25 14:49:03 -0400

    Fix CLI, organize assets by subfolder in content.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var del = require('del');
 var gulp = require('gulp');
 var debug = require('gulp-debug');
 var filter = require('gulp-filter');
-var rename = require('gulp-rename');
 var flatten = require('gulp-flatten');
 var createCurriculumManifest = require('./src/createCurriculumManifest');
 var populateUUIDs = require('./src/populate-uuids');
@@ -17,8 +16,6 @@ var paths = {
 var extensions = require("./asset-extensions")
 var assetsRegex = new RegExp(".*[.](" + extensions.join("|") + ")$");
 
-var sourceDirRegex = new RegExp(paths.source + "\/?");
-
 /* Collects assets folders into one common assets folder */
 gulp.task('assets', function() {
     if (argv["skip-assets"]) {
@@ -30,13 +27,6 @@ gulp.task('assets', function() {
             // -- X -- md is not an acceptable extension, filtered out
             return assetsRegex.test(file.path);
         }))
-        // .pipe(debug({title: "ASSET"}))
-        .pipe(rename(function(path) {
-            // ./content/1.1.1_Assignment_Name/img.png ->
-            // ./1.1.1_Assignment_Name.img.png
-            path.dirname = path.dirname.replace(sourceDirRegex, "");
-        }))
-        // .pipe(debug({title: "ASSET MOD"}))
         .pipe(gulp.dest(paths.build + "/assets"));
     }
 });
@@ -44,16 +34,11 @@ gulp.task('assets', function() {
 /* Builds curriculum.json from structure and contents */
 gulp.task('tree', function() {
     return gulp.src(paths.source + '/structure.xml')
-        // .pipe(debug({title: "STRUCTURE XML"}))
         .pipe(populateUUIDs())
         .pipe(createCurriculumManifest({
           filename: "curriculum.json"
         }))
-        .pipe(rename(function(path) {
-            // ./content/1.1.1_Assignment_Name/img.png ->
-            // ./1.1.1_Assignment_Name.img.png
-            path.dirname = path.dirname.replace(sourceDirRegex, "");
-        }))
+        .pipe(flatten())
         .pipe(gulp.dest(paths.build));
 });
 
