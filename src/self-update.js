39dc63d00099eb7cf40403686e9edaeed4b78425
Olex Ponomarenko <tholex@gmail.com>
2015-02-17 20:34:16 -0500

    Check version properly.

--- src/self-update.js
+++ src/self-update.js
@@ -1,4 +1,5 @@
 var StringDecoder = require('string_decoder').StringDecoder;
+var semver = require('semver');
 var request = require('request');
 
 var runIfLatest = function(run) {
@@ -6,14 +7,24 @@ var runIfLatest = function(run) {
     process.stdout.write("****** Checking for Thunderbird updates... ");
 
     var localVersion = require("./../package.json")["version"];
+    var jsonResp;
     request.get("http://registry.npmjs.org/thunderbird/latest", function(err, body) {
         if (err) {
             process.stdout.write("Error!", err);
             process.exit(1);
         }
         else {
-            console.log("Response from npm", body.body);
-            process.exit(0);
+            jsonResp = JSON.parse(body.body)
+            if (jsonResp && jsonResp["version"]) {
+                if (semver.lt(localVersion, jsonResp["version"])) {
+                    //update
+                } else {
+                    process.stdout.write("Up to date. ******\n");
+                }
+            } else {
+                console.log("Warning: unable to connect to npm. Skipping version check.");
+                run();
+            }
         }
 
     });
