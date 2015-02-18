46857cdcd99030e2d3c7312a6da119573fc9f15e
Olex Ponomarenko <tholex@gmail.com>
2015-02-18 15:19:56 -0500

    Self updater using spawn npm

--- src/self-update.js
+++ src/self-update.js
@@ -1,36 +1,49 @@
+var spawn = require('child_process').spawn;
 var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder("utf8");
 var semver = require('semver');
 var request = require('request');
-var npm = require('npm');
 
 var runIfLatest = function(run) {
+    var package = require("./../package.json")
+    var localVersion = package["version"];
+    var packageName = package["name"]
 
     process.stdout.write("****** Checking for Thunderbird updates... ");
 
-    var localVersion = require("./../package.json")["version"];
     var jsonResp;
-    request.get("http://registry.npmjs.org/thunderbird/latest", function(err, body) {
+    var npmProc;
+    request.get("http://registry.npmjs.org/" + packageName + "/latest", function(err, body) {
         if (err) {
             process.stdout.write("Error fetching version!", err);
             process.exit(1);
         }
+
         else {
             jsonResp = JSON.parse(body.body)
             if (jsonResp && jsonResp["version"]) {
                 if (semver.lt(localVersion, jsonResp["version"])) {
-                    process.stdout.write("Updating! ******\n");
-                    npm.load({}, function(er) {
-                        if (er) {
-                            console.log("Error loading npm!", er);
-                        }
-                        npm.commands.update(["-g", "thunderbird"], function(err) {
-                            if (err) {
-                                console.log("Error updating!", err);
-                            }
-                            console.log(">>>>>> Update finished, re-run the new version! <<<<<<");
-                            process.exit(1);
-                        })
+                    console.log("Updating " + packageName + "... ");
+                    npmProc = spawn("npm", ["update", "-g", packageName]);
+
+                    var handler = function(data) {
+                        process.stdout.write(data ? (decoder.write(data) + decoder.end()) : "");
+                    };
+
+                    npmProc.stdout.on('data', handler);
+                    npmProc.stdout.on('end', handler);
+                    npmProc.stderr.on('data', handler);
+                    npmProc.stderr.on('end', handler);
+
+                    npmProc.on("error", function(errorino) {
+                        console.log("Encountered error:", errorino);
                     });
+
+                    npmProc.on("exit", function(code) {
+                        console.log("\n" + packageName + " update complete. Have a nice day!");
+                        process.exit(code);
+                    });
+
                 } else {
                     process.stdout.write("Up to date. ******\n");
                     run();
