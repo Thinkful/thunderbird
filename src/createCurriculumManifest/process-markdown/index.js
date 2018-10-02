53ccc04b481178aea616084c4ecca05de9b9c3ad
ripleyaffect <t.ripley.peterson@gmail.com>
2018-10-02 09:53:24 -0600

    Updates from #82

--- src/createCurriculumManifest/process-markdown/index.js
+++ src/createCurriculumManifest/process-markdown/index.js
@@ -9,11 +9,22 @@ marked.setOptions({
   renderer,
 });
 
-module.exports = function processMarkdown(markdown) {
-  var html = marked(markdown);
-  var $ = createDom(html);
+/**
+ * Process a blob of markdown
+ *
+ * @param {String} markdown Blob of markdown text to process
+ * @return {String} Html derived from processed markdown
+ */
+module.exports = markdown => {
+  // Turn the markdown into an HTML String using the custom renderer
+  const html = marked(markdown);
 
+  // Turn the HTML String into a jQuery DOM object
+  const $ = createDom(html);
+
+  // Call each task on the jQuery DOM object
   tasks.forEach(task => task($));
 
+  // Return the body as an HTML String
   return $('body').html();
 };
