c7c1ffcd05935e956e9d2ff1bc6e8bfda839baf0
ripleyaffect <t.ripley.peterson@gmail.com>
2018-10-02 20:50:01 -0600

    Fix jsdom call, update version

--- src/create-dom.js
+++ src/create-dom.js
@@ -1,13 +1,13 @@
-var _ = require('lodash');
-var jsdom = require('jsdom').jsdom;
-var jquery = require('jquery');
+const _ = require('lodash');
+const { JSDOM } = require('jsdom');
+const jquery = require('jquery');
 
 module.exports = function(xmlStr) {
-  var html = _.template(
+  const html = _.template(
     '<html><head></head><body> <%= structure %> </body></html>',
     {
       structure: xmlStr,
     }
   );
-  return jquery(jsdom(html).parentWindow);
+  return jquery(new JSDOM(html).window);
 };
